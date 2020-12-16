e2dc1d4eda752b2d190be4518a6bc776babd21d4
XiZhu0204 <x295zhu@uwaterloo.ca>
2020-12-16 03:15:05 -0500

    find empty now returns an instance of a class
    Implemented the empty_location class to improve the readability

--- src/Solver_functions.java
+++ src/Solver_functions.java
@@ -1,6 +1,7 @@
 
 public class Solver_functions {
 	
+
 	static void print_board(int[][] board) {
 		for (int i = 0; i < board.length; i++) {
 			if ( i != 0 && i % 3 == 0) {
@@ -23,31 +24,30 @@ public class Solver_functions {
 	}
 	
 	static boolean solve_sudoku(int[][] board) {
-		int[] empty = find_empty(board);
-		if (empty[0] == -1) {
+		empty_location empty = find_empty(board);
+		if (empty.get_col() == -1) {
 			return true; // filled every blank, solution found
 		}
 		
 		for (int k = 1; k < 10; k++) {
 			if ( check_valid(board, k, empty) ) {
-				board[empty[0]][empty[1]] = k;
+				board[empty.get_row()][empty.get_col()] = k;
 				if (solve_sudoku(board)) {
 					return true;
 				}
-				board[empty[0]][empty[1]] = 0;
 			}
 		}
-		
+		board[empty.get_row()][empty.get_col()] = 0;
 		return false;
 	}
 
-	static int[] find_empty(int[][] board) {
-		int[] result = new int[] {-1,-1};
+	static empty_location find_empty(int[][] board) {
+		empty_location result = new empty_location();
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				if (board[i][j] == 0) {
-					result[0] = i;
-					result[1] = j;
+					result.set_row(i);
+					result.set_col(j);
 					return result; // Row, Column
 				}
 			}
@@ -55,7 +55,7 @@ public class Solver_functions {
 		return result; // nothing found
 	}
 	
-	static boolean check_valid(int[][] board, int num_to_check, int[] location) {
+	static boolean check_valid(int[][] board, int num_to_check, empty_location location) {
 		if (check_col(board, num_to_check, location) && 
 			check_row(board, num_to_check, location) && 
 			check_subgrid(board, num_to_check, location)) 
@@ -66,9 +66,9 @@ public class Solver_functions {
 		}
 	}
 	
-	static boolean check_row(int[][] board, int num_to_check, int[] location) {
+	static boolean check_row(int[][] board, int num_to_check, empty_location location) {
 		for (int i = 0; i < board[0].length; i++) {
-			if ( i != location[1] && board[location[0]][i] == num_to_check) {
+			if ( i != location.get_col() && board[location.get_row()][i] == num_to_check) {
 				//System.out.println("Row fail");
 				return false;
 			}
@@ -76,9 +76,9 @@ public class Solver_functions {
 		return true;
 	}
 	
-	static boolean check_col(int[][] board, int num_to_check, int[] location) {
+	static boolean check_col(int[][] board, int num_to_check, empty_location location) {
 		for (int i = 0; i < board.length; i++) {
-			if ( i != location[0] && board[i][location[1]] == num_to_check) {
+			if ( i != location.get_row() && board[i][location.get_col()] == num_to_check) {
 				//System.out.println("Column fail");
 				return false;
 			}
@@ -86,14 +86,14 @@ public class Solver_functions {
 		return true;
 	}
 	
-	static boolean check_subgrid(int[][] board, int num_to_check, int[] location) {
+	static boolean check_subgrid(int[][] board, int num_to_check, empty_location location) {
 		// Make use of integer division to start the index at the top left most box of the appropriate subgrid
-		final int grid_x_start = (location[1] / 3) * 3;
-		final int grid_y_start = (location[0] / 3) * 3;
+		final int grid_x_start = (location.get_col() / 3) * 3;
+		final int grid_y_start = (location.get_row() / 3) * 3;
 		
 		for (int i = grid_y_start; i < (grid_y_start + 3); i++) {
 			for (int j = grid_x_start; j < (grid_x_start + 3); j++) {
-				if ( ( i != location[0] ) && ( j != location[1]) && board[i][j] == num_to_check) {
+				if ( ( i != location.get_col() ) && ( j != location.get_row()) && board[i][j] == num_to_check) {
 					//System.out.println("Subgrid fail");
 					return false;
 				}
