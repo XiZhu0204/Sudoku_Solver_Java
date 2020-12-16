be099d246db466d200aa92f062f9ff48107a0460
XiZhu0204 <x295zhu@uwaterloo.ca>
2020-12-16 01:51:27 -0500

    Sudoku is now read in from txt file

--- src/Sudoku_Solver.java
+++ src/Sudoku_Solver.java
@@ -1,41 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Sudoku_Solver {
 
 	public static void main(String[] args) {
-		int[][] board = {
-				{ 8, 0, 0, 0, 0, 0, 0, 0, 0},
-				{ 0, 0, 3, 6, 0, 0, 0, 0, 0},
-				{ 0, 7, 0, 0, 9, 0, 2, 0, 0},
-				{ 0, 5, 0, 0, 0, 7, 0, 0, 0},
-				{ 0, 0, 0, 0, 4, 5, 7, 0, 0},
-				{ 0, 0, 0, 1, 0, 0, 0, 3, 0},
-				{ 0, 0, 1, 0, 0, 0, 0, 6, 8},
-				{ 0, 0, 8, 5, 0, 0, 0, 1, 0},
-				{ 0, 9, 0, 0, 0, 0, 4, 0, 0}
-		}; 
-		// Print board tested
-		//Solver_functions.print_board(board);
-		
-		// Find Empty tested
-		//int[] row_col = Solver_functions.find_empty(board);
-		
-		//Check Valid tested
-		
-		/*
-		int[] test = {2,3};
-		System.out.println(board[2][7]);
-		System.out.println(Solver_functions.check_valid(board, 4, test));
-		*/
-		
-		
+		int[][] board = new int[9][9];
+		int row_counter= 0;
+		try {
+			File sudoku = new File("Sudoku.txt");
+			Scanner readin = new Scanner(sudoku);
+
+			while (readin.hasNextLine()) {
+				String[] strArray = readin.nextLine().split(" ");
+				try {
+					for (int i = 0; i < strArray.length; i++) {
+						board[row_counter][i] = Integer.parseInt(strArray[i]);
+					}
+					row_counter++;
+				} catch (NumberFormatException e) {
+					System.out.println("The input file contains a character that is not a number or space.");
+					e.printStackTrace();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("The file \"Sudoku.txt\" is missing.");
+			e.printStackTrace();
+		}
+
 		if (Solver_functions.solve_sudoku(board)) {
 			System.out.println("Sudoku Solved!");
 			Solver_functions.print_board(board);
 		} else {
 			System.out.println("The Sudoku is unsolvable.");
 		}
-		
-		
 	}
 }
 
